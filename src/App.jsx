2331c6a9715241d358836f5547cbb6f52e407d58
Miel John A. Arnigo <mjarnigo123@gmail.com>
2024-11-26 14:09:54 +0800

    teacher management e edit pani

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import UserManagementPage from './pages/UserManagementPage';
 import DashboardPage from './pages/DashboardPage';
+import UserManagementPage from './pages/UserManagementPage';
+import TeacherManagementPage from './pages/TeacherManagementPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/user-management" element={<UserManagementPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/user-management" element={<UserManagementPage />} />
+        <Route path="/teacher-management" element={<TeacherManagementPage />} />
       </Routes>
     </Router>
   );
