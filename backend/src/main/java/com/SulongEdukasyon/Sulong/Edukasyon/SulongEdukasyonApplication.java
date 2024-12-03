d928c42c6088c786ba5945d27651a07b88ac33ca
Adrian AJ R. De Leon <115869081+Adrian6204@users.noreply.github.com>
2024-12-04 00:30:13 +0800

    added frontend and backend folders for better app structure

---
+++ backend/src/main/java/com/SulongEdukasyon/Sulong/Edukasyon/SulongEdukasyonApplication.java
@@ -0,0 +1,13 @@
+package com.SulongEdukasyon.Sulong.Edukasyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulongEdukasyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulongEdukasyonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/SulongEdukasyon/Sulong/Edukasyon/SulongEdukasyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SulongEdukasyon.Sulong.Edukasyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulongEdukasyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
