2ffe1017b7bbf99ce2e5650af2547086649e8c99
Miel John A. Arnigo <mjarnigo123@gmail.com>
2024-12-12 22:26:13 +0800

    teacher new

--- frontend/src/view/components/TeacherManagement.jsx
+++ frontend/src/view/components/TeacherManagement.jsx
@@ -6,20 +6,13 @@ const TeacherManagement = () => {
     <div className="teacher-management">
       <div className="header">
         <h1>Teacher Management</h1>
-        <button className="logout-button">Logout</button>
       </div>
       <div className="content">
-        <div className="card-grid">
-          <div className="card">Class List</div>
-          <div className="card">Activity Details</div>
-          <div className="card">View Progress and Score</div>
-        </div>
-        <div className="student-leaderboard">
-          <h2>Student Leaderboard</h2>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
+      <div className="button-grid">
+  <button className="card-button">Class List</button>
+  <button className="card-button">Activity Details</button>
+  <button className="card-button">View Progress and Score</button>
+</div>
       </div>
     </div>
   );
