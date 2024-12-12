3faed996495862350c907823c4431c64774916e9
Miel John A. Arnigo <mjarnigo123@gmail.com>
2024-12-12 23:01:41 +0800

    connected

--- frontend/src/view/components/Sidebar.jsx
+++ frontend/src/view/components/Sidebar.jsx
@@ -29,7 +29,7 @@ const Sidebar = () => {
             </Link>
           </li>
           <li>
-            <a href="#rooms">
+            <a href="/joinroom">
               <img src={calendarLogo} alt="Rooms" />
               Rooms
             </a>
