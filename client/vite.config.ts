b4fdc7fd41398a93e47d8aaef026512fe1bd843c
Anton Joshua Dominguez <143872285+antonjoshuadominguez@users.noreply.github.com>
2025-04-21 00:01:19 +0800

    Fix Vercel deployment paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: './client',
   build: {
-    outDir: './dist/public',
+    outDir: '../client/dist/public',
     emptyOutDir: true
   }
 })
